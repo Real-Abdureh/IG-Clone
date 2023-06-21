4e84743034c957df23539d94a7c90e5640703789
ABDULRAHMAN ABBAS <abdouliabbas75@gmail.com>
2023-06-21 10:24:33 +0100

    authentication

--- userauth/forms.py
+++ userauth/forms.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from userauth.models import Profile
 from django import forms
+
 
 
 class EditProfileForm(forms.ModelForm):
@@ -13,5 +16,18 @@ class EditProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['picture', 'first_name', 'last_name', 'bio', 'url', 'location']
+
+class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username', 'class': 'prompt srch_explore'}), max_length=50, required=True)
+    # username = forms.EmailInput(widget=forms.TextInput(attrs={'placeholder': 'Username'}), max_length=50, required=True)
+
+    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email', 'class': 'prompt srch_explore'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter Password', 'class': 'prompt srch_explore'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password', 'class': 'prompt srch_explore'}))
+    # email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
         
 
--- userauth/views.py
+++ userauth/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator
 from userauth.models import Profile
-from userauth.forms import EditProfileForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from userauth.forms import EditProfileForm, UserRegisterForm
 from django.urls import resolve, reverse
 from post.models import Post, Follow, Stream
 from django.db import transaction 
@@ -120,6 +122,33 @@ def editProfile(request):
     }
     return render(request, 'edit-profile.html', context)
 
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            # Profile.get_or_create(user=request.user)
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Hurray your account was created!!')
+
+            # Automatically Log In The User
+            new_user = authenticate(username=form.cleaned_data['username'],
+                                    password=form.cleaned_data['password1'],)
+            login(request, new_user)
+            # return redirect('editprofile')
+            return redirect('index')
+            
+
+
+    elif request.user.is_authenticated:
+        return redirect('index')
+    else:
+        form = UserRegisterForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'sign-up.html', context)
 
 
 
